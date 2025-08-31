53f1450aadc868d010c1333ad0e38b84cbeb1355
Pro-divin <divin2250@gmail.com>
2025-08-31 20:04:07 +0200

    updates settings

--- greatKart/settings.py
+++ greatKart/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fc2sf4)8!32ode6wwenm_7yx5giiw9s@a*32buxz!%dbc!k9kz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['greatkart-django-7.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
