e9fb10bf829e974897aa3b449221f2827069f920
Pro-divin <divin2250@gmail.com>
2025-09-01 15:12:42 +0200

    ad postgress_sql

--- greatKart/settings.py
+++ greatKart/settings.py
@@ -83,8 +83,12 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD', ),
+        'HOST': config('HOST'), 
+        'PORT': '5432'
     }
 }
 
