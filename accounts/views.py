13f3c51aa63a01bccc758fb7a79c54aa2ddf6120
Pro-divin <divin2250@gmail.com>
2025-09-05 22:09:09 +0200

    update

--- accounts/views.py
+++ accounts/views.py
@@ -24,42 +24,59 @@ import requests
 
 
 @never_cache
+@never_cache
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
+            # Get form data
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
             phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             username = email.split("@")[0]
-            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+
+            # Create user
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password
+            )
             user.phone_number = phone_number
+            user.is_active = False  # keep inactive until email activation
             user.save()
 
-            # Email activation
+            # Prepare email
             current_site = get_current_site(request)
-            mail_subject = 'Please activate your account'
-            message = render_to_string('accounts/account_verification_email.html', {
+            mail_subject = 'Activate Your Account'
+            
+            html_content = render_to_string('accounts/account_verification_email.html', {
                 'user': user,
                 'domain': current_site,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': default_token_generator.make_token(user),
             })
-            to_email = email
-            send_email = EmailMessage(mail_subject, message, to=[to_email])
-            send_email.send()
+            text_content = strip_tags(html_content)  # fallback plain text
 
-            # Render success template
+            email_message = EmailMultiAlternatives(
+                mail_subject,
+                text_content,
+                to=[email]
+            )
+            email_message.attach_alternative(html_content, "text/html")
+            email_message.send()
+
+            # Success page
             return render(request, 'accounts/registration_success.html', {'user': user})
     else:
         form = RegistrationForm()
 
-    context = {
-        'form': form,
-    }
+    context = {'form': form}
     return render(request, 'accounts/register.html', context)
+
 
 
 def login(request):
