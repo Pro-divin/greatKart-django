5f083b1643bf77f94280f3ac4f0bb5bd69c4054b
Pro-divin <divin2250@gmail.com>
2025-09-05 13:25:54 +0200

    update

--- accounts/views.py
+++ accounts/views.py
@@ -15,6 +15,9 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
+from django.core.mail import EmailMultiAlternatives
+from django.utils.html import strip_tags
+
 import requests
 
 
@@ -41,15 +44,21 @@ def register(request):
 
             # Email activation
             current_site = get_current_site(request)
-            mail_subject = 'Please activate your account'
-            message = render_to_string('accounts/account_verification_email.html', {
-                'user': user,
-                'domain': current_site,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': default_token_generator.make_token(user),
+
+            subject = 'Please activate your account'
+            to_email = user.email
+            from_email = 'noreply@jurucollection.com'
+            
+            html_content = render_to_string('accounts/account_verification_email.html', {
+                'user_name': user.first_name,
+                'verification_link': f"http://{current_site.domain}/accounts/activate/{urlsafe_base64_encode(force_bytes(user.pk))}/{default_token_generator.make_token(user)}/"
             })
-            send_email = EmailMessage(mail_subject, message, to=[email])
-            send_email.send()
+            
+            text_content = strip_tags(html_content)  # fallback for non-HTML clients
+            
+            email = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+            email.attach_alternative(html_content, "text/html")  # attach HTML version
+            email.send()
 
             # Render success template with no-cache headers
             response = render(request, 'accounts/registration_success.html', {'user': user})
